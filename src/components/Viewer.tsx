031f32ed0a876f3ff35e65c364636dadf5fb8059
origamizyt <zhaoyitong18@163.com>
2025-08-22 03:34:45 +0800

    feat: add sidebar animation

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -102,44 +102,41 @@ export default function Viewer(props: ViewerProps) {
 
   return (
     <div className='flex h-full'>
-      {
-        showSidebar &&
-        <div className="w-1/5 border-base-300 border-r-1 overflow-y-auto">
-          <h1 className="text-xl font-black p-5 pb-3">目录</h1>
-          {
-            initialized ?
-            <ul className="menu w-full">
-              {outline.map(item => 
-                <li key={item.title}>
-                  {
-                    item.items.length ? 
-                    <details>
-                      <summary><a href='#' onClick={() => gotoDest(item.dest)}>{ item.title }</a></summary>
-                      <ul>
-                        { item.items.map((subitem: any) => 
-                          <li key={subitem.title}><a href='#' onClick={() => gotoDest(subitem.dest)}>{ subitem.title }</a></li>
-                        )}
-                      </ul>
-                    </details> :
-                    <a href='#' onClick={() => gotoDest(item.dest)}>{ item.title }</a>
-                  }
-                </li>
-              )}
-            </ul> :
-            <div className='flex w-full flex-col gap-2 px-5 mt-3'>
-              <div className='skeleton h-6'></div>
-              <div className='skeleton h-6 ml-8'></div>
-              <div className='skeleton h-6 ml-8'></div>
-              <div className='skeleton h-6'></div>
-              <div className='skeleton h-6 ml-8'></div>
-              <div className='skeleton h-6 ml-8'></div>
-              <div className='skeleton h-6'></div>
-              <div className='skeleton h-6 ml-8'></div>
-              <div className='skeleton h-6 ml-8'></div>
-            </div>
-          }
-        </div>
-      }
+      <div className="w-1/5 border-base-300 border-r-1 overflow-y-auto transition-all" style={{ marginLeft: showSidebar ? 0 : '-20%'}}>
+        <h1 className="text-xl font-black p-5 pb-3">目录</h1>
+        {
+          initialized ?
+          <ul className="menu w-full">
+            {outline.map(item => 
+              <li key={item.title}>
+                {
+                  item.items.length ? 
+                  <details>
+                    <summary><a href='#' onClick={() => gotoDest(item.dest)}>{ item.title }</a></summary>
+                    <ul>
+                      { item.items.map((subitem: any) => 
+                        <li key={subitem.title}><a href='#' onClick={() => gotoDest(subitem.dest)}>{ subitem.title }</a></li>
+                      )}
+                    </ul>
+                  </details> :
+                  <a href='#' onClick={() => gotoDest(item.dest)}>{ item.title }</a>
+                }
+              </li>
+            )}
+          </ul> :
+          <div className='flex w-full flex-col gap-2 px-5 mt-3'>
+            <div className='skeleton h-6'></div>
+            <div className='skeleton h-6 ml-8'></div>
+            <div className='skeleton h-6 ml-8'></div>
+            <div className='skeleton h-6'></div>
+            <div className='skeleton h-6 ml-8'></div>
+            <div className='skeleton h-6 ml-8'></div>
+            <div className='skeleton h-6'></div>
+            <div className='skeleton h-6 ml-8'></div>
+            <div className='skeleton h-6 ml-8'></div>
+          </div>
+        }
+      </div>
       <div className="flex w-full flex-col overflow-hidden relative">
         <div className="flex gap-2 p-3 justify-center z-100">
           {
@@ -147,24 +144,24 @@ export default function Viewer(props: ViewerProps) {
               <button className="btn btn-sm absolute top-3 left-3 z-10" onClick={() => setShowSidebar(!showSidebar)}>
                 {
                   showSidebar ? 
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M17.488 4.43a.75.75 0 0 1 .081 1.058L11.988 12l5.581 6.512a.75.75 0 1 1-1.139.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.058-.081Zm-4 0a.75.75 0 0 1 .081 1.058L7.988 12l5.581 6.512a.75.75 0 1 1-1.138.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.057-.081Z" clip-rule="evenodd"/></svg>
-                  : <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M6.512 4.43a.75.75 0 0 1 1.057.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.138-.976L12.012 12L6.431 5.488a.75.75 0 0 1 .08-1.057Zm4 0a.75.75 0 0 1 1.058.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.14-.976L16.013 12l-5.581-6.512a.75.75 0 0 1 .081-1.057Z" clip-rule="evenodd"/></svg> 
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M17.488 4.43a.75.75 0 0 1 .081 1.058L11.988 12l5.581 6.512a.75.75 0 1 1-1.139.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.058-.081Zm-4 0a.75.75 0 0 1 .081 1.058L7.988 12l5.581 6.512a.75.75 0 1 1-1.138.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.057-.081Z" clipRule="evenodd"/></svg>
+                  : <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M6.512 4.43a.75.75 0 0 1 1.057.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.138-.976L12.012 12L6.431 5.488a.75.75 0 0 1 .08-1.057Zm4 0a.75.75 0 0 1 1.058.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.14-.976L16.013 12l-5.581-6.512a.75.75 0 0 1 .081-1.057Z" clipRule="evenodd"/></svg> 
                 }
               </button>
               <button className="btn btn-ghost btn-sm" onClick={() => flipRef.current.pageFlip().turnToPage(0)} disabled={currentPage === 0 || flipping}> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="currentColor"><path d="M17.75 19a.75.75 0 0 1-1.32.488l-6-7a.75.75 0 0 1 0-.976l6-7A.75.75 0 0 1 17.75 5v14Z" opacity=".5"/><path fill-rule="evenodd" d="M13.488 19.57a.75.75 0 0 0 .081-1.058L7.988 12l5.581-6.512a.75.75 0 1 0-1.138-.976l-6 7a.75.75 0 0 0 0 .976l6 7a.75.75 0 0 0 1.057.082Z" clip-rule="evenodd"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="currentColor"><path d="M17.75 19a.75.75 0 0 1-1.32.488l-6-7a.75.75 0 0 1 0-.976l6-7A.75.75 0 0 1 17.75 5v14Z" opacity=".5"/><path fillRule="evenodd" d="M13.488 19.57a.75.75 0 0 0 .081-1.058L7.988 12l5.581-6.512a.75.75 0 1 0-1.138-.976l-6 7a.75.75 0 0 0 0 .976l6 7a.75.75 0 0 0 1.057.082Z" clipRule="evenodd"/></g></svg>
               </button>
               <button className="btn btn-sm btn-ghost" onClick={previousPage} disabled={currentPage <= 0 || flipping}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M10.53 5.47a.75.75 0 0 1 0 1.06l-4.72 4.72H20a.75.75 0 0 1 0 1.5H5.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M10.53 5.47a.75.75 0 0 1 0 1.06l-4.72 4.72H20a.75.75 0 0 1 0 1.5H5.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clipRule="evenodd"/></svg>
               </button>
               <div className="flex flex-col justify-center">
                 <span className="text-sm font-light">{ currentPage + 1 } of { pages.length }</span>
               </div>
               <button className="btn btn-sm btn-ghost" onClick={nextPage} disabled={currentPage >= pages.length - 1 || flipping}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M13.47 5.47a.75.75 0 0 1 1.06 0l6 6a.75.75 0 0 1 0 1.06l-6 6a.75.75 0 1 1-1.06-1.06l4.72-4.72H4a.75.75 0 0 1 0-1.5h14.19l-4.72-4.72a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M13.47 5.47a.75.75 0 0 1 1.06 0l6 6a.75.75 0 0 1 0 1.06l-6 6a.75.75 0 1 1-1.06-1.06l4.72-4.72H4a.75.75 0 0 1 0-1.5h14.19l-4.72-4.72a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"/></svg>
               </button>
               <button className="btn btn-ghost btn-sm" onClick={() => flipRef.current.pageFlip().turnToPage(pages.length - 1)} disabled={currentPage === pages.length - 1 || flipping}> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="currentColor"><path d="M6.25 19a.75.75 0 0 0 1.32.488l6-7a.75.75 0 0 0 0-.976l-6-7A.75.75 0 0 0 6.25 5v14Z" opacity=".5"/><path fill-rule="evenodd" d="M10.512 19.57a.75.75 0 0 1-.081-1.058L16.012 12l-5.581-6.512a.75.75 0 1 1 1.139-.976l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.058.082Z" clip-rule="evenodd"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="currentColor"><path d="M6.25 19a.75.75 0 0 0 1.32.488l6-7a.75.75 0 0 0 0-.976l-6-7A.75.75 0 0 0 6.25 5v14Z" opacity=".5"/><path fillRule="evenodd" d="M10.512 19.57a.75.75 0 0 1-.081-1.058L16.012 12l-5.581-6.512a.75.75 0 1 1 1.139-.976l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.058.082Z" clipRule="evenodd"/></g></svg>
               </button>
               <input type="text" className="input input-sm max-w-[100px]" placeholder="Page to Go" value={gotoText} onChange={e => setGotoText(e.target.value)}/>
               <button className="btn btn-primary btn-sm" disabled={!isFinite(gotoPage) || gotoPage <= 0 || gotoPage > pages.length || flipping} onClick={goto}>Go</button>
@@ -173,8 +170,7 @@ export default function Viewer(props: ViewerProps) {
         </div>
         <div className="flex-grow flex justify-center relative" ref={viewportRef} style={{ transform: initialized ? `translateY(${YTRANSLATE}px)`: 'none' }}>
           {
-            initialized ?
-            undefined :
+            !initialized && 
             <div className='w-full p-10 absolute h-full'>
               <div className='skeleton w-full h-full'></div>
             </div>
